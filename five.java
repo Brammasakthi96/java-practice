d587eead228704cfd98090f714a2fefcfeacfac4
Brammasakthi96 <greenwaynrjnmari1996@gmail.com>
2026-09-20 01:03:40 +0530

    Add files via upload

---
+++ five.java
@@ -0,0 +1,16 @@
+package sat;
+                                                                 //FOR LOOP QUESTION NO.1
+public class five {
+                                                                //USING FOR LOOP TO PRINT EVEN NUMBER BETWEEN 1 TO 100
+	public static void main(String[] args) {
+		                                                         
+		System.out.println("even numbers between 1 to 100");
+		for (int i=1;i<=100;i++)                                  
+		{
+			if(i%2==0)
+			{
+			System.out.println("Even number "+i);
+		}
+		}
+     	}
+}
